4f31b69c636a8540d0a455b5ae908bfa5ca5b609
Håkon Ødegård Løvdal <me@hakloev.no>
2017-04-06 13:05:38 +0200

    Initial Commit

---
+++ Foodz/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Foodz
+//
+//  Created by Håkon Ødegård Løvdal on 06/04/2017.
+//  Copyright © 2017 Håkon Ødegård Løvdal. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
